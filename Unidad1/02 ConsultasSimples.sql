f8923d1981760eb98be7f8f89225ab2369f4dba3
GRLuis1565 <luisggr2004@gmail.com>
2025-01-28 19:55:21 -0600

    Se agregaron nuevas consultas de test de comparacion, operadores relacionalees

--- Unidad1/02 ConsultasSimples.sql
+++ Unidad1/02 ConsultasSimples.sql
@@ -32,6 +32,13 @@ select *,(UnitPrice * Quantity) as Importe from [Order Details]
 -- Seleccionar las fechas de orden ano mes y dia, el cliente que lo ordeno y el empleado que lo realizo
 select OrderDate as Fecha, year(OrderDate) as Ano, month(OrderDate) as Mes, day(OrderDate) as Dia, CustomerID, EmployeeID from Orders
 
+-- Filas duplicadas (Distinct)
+select * from Customers;
+
+-- Mostrar los paises en donde se tienen clientes
+select distinct Country as Pais from Customers
+order by Country
+
 -- Clausula where 
 -- Operadores relacionales (<, >, <=, >=, != o <>)
 select * from Customers
@@ -50,3 +57,18 @@ select ProductName as 'Nombre del Producto', CategoryID as Categoria, UnitsInSto
 -- Seleccionar ordenes de compra mostrando la fecha de orden, entrega, envio, cliente al que se vendio de 1996
 select OrderDate as 'Fecha de Orden', RequiredDate as Entrega, ShippedDate as Envio, CustomerID  from Orders where year(OrderDate) = 1996
 select * from Orders
+
+-- mostrar todas las ordenes de compra donde la cantidad de productos comprados sea mayor a 5 
+select Quantity as Ordenes from [Order Details] where Quantity >= 40;
+
+-- mostarr el nomre completo del empleado su numero de empleado fecha de nacimimiento la ciudad y fecha de contratacion y 
+-- esta deben de ser de aquellos que fueron contratados despues de 1993, los resultados en sus encabesados deben de mostrarlos en español
+select LastName as Apellido, FirstName as Nombre, EmployeeID as 'ID empelado', BirthDate 'Fecha de nacimiento', City as Ciudad, HireDate as 'Fecha de contratacion' from Employees where year(HireDate) >1993
+select (FirstName + '' + LastName) as 'Nombre completo', EmployeeID as 'ID empelado', BirthDate 'Fecha de nacimiento', City as Ciudad, HireDate as 'Fecha de contratacion' from Employees where year(HireDate) >1993
+select CONCAT(LastName, '',FirstName, ' - ', Title) as [Nombre Completo], EmployeeID as 'ID empelado', BirthDate 'Fecha de nacimiento', City as Ciudad, HireDate as 'Fecha de contratacion' from Employees where year(HireDate) >1993
+
+-- Mostrar los empleados que no son dirijidos por el jefe fuller Andrew
+select (FirstName + '' + LastName) as 'Nombre completo', EmployeeID as 'ID empelado' from Employees where ReportsTo != 2
+
+-- seleccionar todos los empleados que no tienen jefe 
+select * from Employees where ReportsTo is null
